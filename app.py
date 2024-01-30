bf724b2c06e7891f409aa59e934d744e959db3e5
SteffanMooreThreeUK <steffan.ynyrmoore@three.co.uk>
2024-01-30 14:35:54 +0000

    Poisson distribution can find success numbers
    The calculation for finding the number of successes given a mean and probability now successfully returns values.

--- app.py
+++ app.py
@@ -26,6 +26,12 @@ def ensure_start_conditions(function):
 
 @ensure_start_conditions
 def poisson_probability(mean = None, success_number = None, probability = None):
+    """
+    Uses three of its arguments to find the value for the other according to the Poisson distribution and returns
+    them all as a dictionary. Mean is the number of expected successes, success_number is the actual number of
+    successes and probability is the likelyhood of there being that many successes given the expected number. This
+    is used for a discrete data set so doesn't work on continuous data.
+    """
     
     arguments = {"mean": mean, "success_number": success_number, "probability": probability}
     
@@ -44,16 +50,43 @@ def poisson_probability(mean = None, success_number = None, probability = None):
     match missing_arg:
 
         case "mean":
+            success_number = int(success_number)
             print("mean")
 
         case "success_number":
-            print("success_number")
+            
+            closest_to_target_value = None
+            temp_success_value = int(mean)
+            value_to_find = math.exp(-mean) / probability
+
+            # Increases the number of successes until both sides of the equation match
+            while True:
+                current_value = (math.factorial(temp_success_value) / (mean ** temp_success_value))
+                value_difference = abs(value_to_find - current_value)
+
+                if closest_to_target_value == None or closest_to_target_value > value_difference:
+                    closest_to_target_value = value_difference
+                elif closest_to_target_value <= value_difference:
+                    break
+
+                temp_success_value += 1
+
+            # Due to symettry, the distribution has an upper and lower possible value
+            closest_upper_success_number = (temp_success_value - 1)
+            closest_lower_success_number = 2 * int(mean) - (temp_success_value - 1)
+
+            # Number of successes has to be positive so the lower bracket can be discounted in some cases.
+            if closest_lower_success_number < 0:
+                success_number = [closest_upper_success_number]
+            else:
+                success_number = [closest_lower_success_number, closest_upper_success_number]
 
         case "probability":
+
+            success_number = int(success_number)
             probability = ((mean ** success_number) * math.exp(-mean)) / math.factorial(success_number)
-            print("probability")
 
-
+    return {"mean": mean, "success_number": success_number, "probability": probability}
     """
     for argument in signature(poisson_probability).parameters:
         print(argument)
@@ -86,4 +119,4 @@ if __name__ == "__main__":
 
 """
 
-print(poisson_probability(mean = "five", success_number = "less"))
+print(poisson_probability(mean = 5.2, probability = 0.15))
