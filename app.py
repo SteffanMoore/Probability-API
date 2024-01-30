23ed132599bfc3b32dc486201765627e064220e1
SteffanMooreThreeUK <steffan.ynyrmoore@three.co.uk>
2024-01-30 12:00:12 +0000

    Continued building of Poisson function
    Logging has now been implemented and the Poisson function now checks that the correct data types are used. This needs to be improved with increased specificity - currently it allows all floats and ints but this isn't correct. Further work needs to be done to add other calculations to the function (so far the only working function is that for probability).

--- app.py
+++ app.py
@@ -1,33 +1,60 @@
 import math
+import logging
 from inspect import signature, getfullargspec
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+
+logging.basicConfig(level = logging.INFO)
 
 def ensure_start_conditions(function):
     """
     Decorator function to ensure that there is no more and no less that one function argument which has no value.
     """
 
-    def wrapper(*args, **kwargs):
+    def wrapper(**kwargs):
 
-        if len(args) > 0:
-            print("Only keyword arguments should be handled by this function")
+        arg_no = len(signature(function).parameters) - 1
+
+        if len(kwargs) != arg_no:
+            logging.info(f"This function requires {arg_no} arguments.")
             return False
         
-        min_args = len(signature(function).parameters) - 1
-
-        if len(kwargs) < min_args:
-            print(f"This function requires at least {min_args} arguments.")
-            return False
-        
-        function(*args, **kwargs)
+        return function(**kwargs)
 
     return wrapper
 
 
 @ensure_start_conditions
-def poisson_probability(mean = None, success_number = None, failure_number = None, probability = None):
+def poisson_probability(mean = None, success_number = None, probability = None):
     
+    arguments = {"mean": mean, "success_number": success_number, "probability": probability}
+    
+    # Finds the argument left as default and checks that the correct data types are used.
+    for arg in arguments:
+
+        if arguments[arg] == None:
+            missing_arg = arg
+
+        elif not isinstance(arguments[arg], (int, float)):
+            logging.info(f"""Provided argument - "{arguments[arg]}" for {arg} is not the correct data type. It """
+                         f"""is currently a {type(arguments[arg])}.""")
+            return False
+
+    # Function changes depending on which argument is missing.
+    match missing_arg:
+
+        case "mean":
+            print("mean")
+
+        case "success_number":
+            print("success_number")
+
+        case "probability":
+            probability = ((mean ** success_number) * math.exp(-mean)) / math.factorial(success_number)
+            print("probability")
+
+
+    """
     for argument in signature(poisson_probability).parameters:
         print(argument)
 
@@ -41,6 +68,7 @@ def poisson_probability(mean = None, success_number = None, failure_number = Non
 
     #print(signature(poisson_probability).parameters)
     #print(argument for argument in signature(poisson_probability).parameters.keys)
+    """
 
 """
 app = Flask(__name__)
@@ -58,4 +86,4 @@ if __name__ == "__main__":
 
 """
 
-poisson_probability(mean="five")
+print(poisson_probability(mean = "five", success_number = "less"))
